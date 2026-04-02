93ef47f09271b691125d8bd4a617abaadc491f10
Claudio Zopfi <35420455+misuco@users.noreply.github.com>
2026-04-02 02:09:36 +0200

    Created js/osc-interface.js with OSC message logging functionality

---
+++ js/osc-interface.js
@@ -0,0 +1,10 @@
+// osc-interface.js
+
+// OSC message logging to browser console
+
+function logOSCMessage(message) {
+    console.log('Received OSC Message:', message);
+}
+
+// Example usage
+// logOSCMessage({'/example/address': 42});
